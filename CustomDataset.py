9b6a6e3c683b556d40a7d85554e4eba17ca9cddd
yunsang <songys96@naver.com>
2021-07-05 21:49:28 +0900

    refactoring

--- CustomDataset.py
+++ CustomDataset.py
@@ -6,7 +6,12 @@ class CustomDataset():
         self.checklist = []
         self.errors = []
 
+
     def loadBagLabel(self, bag_folder):
+        """
+            bag 폴더 안에 있는 label.txt 파일을 읽어서 행동별 시작 인덱스 리스트를 반환합니다
+            return 값 : {행동1 : [21, 56, 23], 행동2 : [45, ...]}
+        """
         acts = {}
         try:
             with open(os.path.join(bag_folder, "label.txt"), "r") as f:
@@ -20,9 +25,16 @@ class CustomDataset():
         except:
             self.errors.append(bag_folder)
             print(bag_folder, " - label.txt is not correct")
+
         return acts
 
+
     def checkList(self, save_folder):
+        """
+            save_folder에 있는 checklist.txt를 읽고 
+            self.checklist에 bag_id를 append합니다
+            # bag_id : 210702_145232 의 형식 (날짜_고유번호)
+        """
         self.checklist = []
         checklist = []
         checklist_file = os.path.join(save_folder, "checklist.txt")
@@ -38,24 +50,35 @@ class CustomDataset():
         return checklist
 
     def checkDuplicate(self, bag_id):
+        """
+            bag_id가 self.checklist에 있는지 검사
+        """
         dup = False
         if bag_id in self.checklist:
             dup = True
+
         return dup
 
-    def addBagOnCheckList(self,save_folder, bag_id):
+
+    def addBagIdOnCheckList(self,save_folder, bag_id):
+        """
+        copy를 완료하면 기존의 checklist.txt 파일을 업데이트합니다
+        """
         self.checklist.append(bag_id)
         checklist_file = os.path.join(save_folder, "checklist.txt")
         if os.path.exists(checklist_file):
             with open(checklist_file, "a") as f:
                 f.write(f"{bag_id}\n")
 
+
     def extractBag(self, folder):
         '''
+        삭제 예정
         acts -> {sit:[21, 37], walk: [221,232]}
         idxs -> [21, 37]
-        filenames -> [21.jpg,22.jpg,23.jpg, ... , 50.jpg]
+        Return acts_ext -> {sit:[00021, 00037], walk: [00221, 00232]}
         '''
+
         acts = self.loadBagLabel(folder)
         acts_ext = {}
         if not acts:
@@ -65,7 +88,7 @@ class CustomDataset():
             act_ext = []
             idxs = acts[act]
             for start_idx in idxs:
-                filename = self.extractFilename(start_idx)
+                filename = str(int(start_idx)).zfill(5)       # 21 -> 00021
                 # color_set, depth_set = self.packageFolder(folder, filenames)
                 act_ext.append(filename)
             acts_ext[act] = act_ext
@@ -74,7 +97,7 @@ class CustomDataset():
 
         return acts_ext
 
-    def copyBag(self, from_folder, save_folder, frames=30):
+    def createCustomBag(self, from_folder, save_folder, frames=30):
         ''' 
         save_folder should be DatasetFolder
         ex. /User/CustomDataset/
@@ -110,15 +133,17 @@ class CustomDataset():
         print("-----Copy Finished-----")
         self.addBagOnCheckList(save_folder, bag_id)
 
-    def copyBags(self, from_folder, save_folder):
+
+    def createCustomBags(self, from_folder, save_folder):
         id = 0
         for bag in os.listdir(from_folder):
             if "." in bag:
                 continue
             id += 1
             bag_file = from_folder+"/"+bag
-            self.copyBag(bag_file, save_folder)
+            self.createCustomBag(bag_file, save_folder)
         print(sorted(self.errors))
+
 
     @staticmethod
     def checkId(save_folder, act):
@@ -129,6 +154,7 @@ class CustomDataset():
                 if id > max_id:
                     max_id = id
         return max_id            
+        
 
     @staticmethod
     def copyFile(from_folder, save_folder, filename):
@@ -147,6 +173,7 @@ class CustomDataset():
         shutil.copyfile(src_depth, dst_depth)
         shutil.copyfile(src_info, dst_info)
 
+
     @staticmethod
     def createDirectory(save_folder):
         if not os.path.exists(save_folder):
@@ -163,12 +190,20 @@ class CustomDataset():
         #     os.mkdir(os.path.join(save_folder, "depth"))
         # return
 
+
     @staticmethod
     def extractFilename(start_idx):
+        """
+        삭제 예정
+        """
         return str(int(start_idx)).zfill(5)
+
 
     @staticmethod
     def extractFilenames(start_idx, frames=30):
+        """
+        삭제 예정
+        """
         print("extract File names")
         filenames = []
         for i in range(frames):
@@ -176,9 +211,13 @@ class CustomDataset():
             filenames.append(filename)
         return filenames
 
+
     @staticmethod
     def joinFolder(folder, name="color"):
-        ''' type = [ 'color', 'depth' ] '''
+        ''' 
+        삭제예정
+        type = [ 'color', 'depth' ] 
+        '''
         if not name in ['color', 'depth']: 
             raise TypeError
 
@@ -189,9 +228,13 @@ class CustomDataset():
 
         return joinFunc
 
+
     @staticmethod
     def packageFolder(folder, filenames):
-        ''' copy_fileset -> [(from_file1, file1), (from_file2, file2), ..] '''
+        '''
+        삭제 예정 
+        copy_fileset -> [(from_file1, file1), (from_file2, file2), ..] 
+        '''
         color_copy_set = list(map(CustomDataset.joinFolder(folder, "color"), filenames))
         depth_copy_set = list(map(CustomDataset.joinFolder(folder, "depth"), filenames))
         return color_copy_set, depth_copy_set
@@ -202,5 +245,5 @@ save_folder = "/Users/song-yunsang/Desktop/Business/Butler/Dataset/CustomDataset
 cd = CustomDataset()
 # cd.extractFilename(10)
 # cd.copyBag("/Users/song-yunsang/Desktop/Business/Butler/Dataset/bags/20210702_154436", "/Users/song-yunsang/Desktop/Business/Butler/Dataset/CustomDataset/")
-cd.copyBags(from_folder, save_folder)
+cd.createCustomBags(from_folder, save_folder)
 
